03a64e4994f1b885fa7015812c53183d87befada
MattEsales <gordon-matt@users.noreply.github.com>
2015-02-24 05:33:47 +0000

    Some work done to get image picker (elFinder) showing in TinyMCE. Not finished yet.

--- Kore.Web.ContentManagement/Areas/Admin/Pages/Scripts/index.js
+++ Kore.Web.ContentManagement/Areas/Admin/Pages/Scripts/index.js
@@ -535,27 +535,6 @@ var ViewModel = function () {
             Slug: { required: true, maxlength: 255 }
         }
     });
-
-    self.tinyMCEConfig = {
-        theme: "modern",
-        plugins: [
-            "advlist autolink lists link image charmap print preview hr anchor pagebreak",
-            "searchreplace wordcount visualblocks visualchars code fullscreen",
-            "insertdatetime media nonbreaking save table contextmenu directionality",
-            "emoticons template paste textcolor"
-        ],
-        toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
-        toolbar2: "print preview media | forecolor backcolor emoticons",
-        image_advtab: true,
-        templates: [
-            { title: 'Test template 1', content: 'Test 1' },
-            { title: 'Test template 2', content: 'Test 2' }
-        ],
-        content_css: tinyMCEContentCss,
-        force_br_newlines: false,
-        force_p_newlines: false,
-        forced_root_block: ''
-    };
 };
 
 var viewModel;

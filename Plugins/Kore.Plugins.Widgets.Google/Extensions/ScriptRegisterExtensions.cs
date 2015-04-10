5e23b7498b8a3e45b05ca27913c0f57395730532
MattEsales <gordon-matt@users.noreply.github.com>
2015-04-10 08:49:15 +0000

    New Google Widget. So far have Google Maps.. will add Analytics & Sitemap Generator

---
+++ Plugins/Kore.Plugins.Widgets.Google/Extensions/ScriptRegisterExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Kore.Web.Mvc.Resources;
+
+namespace Kore.Plugins.Widgets.Google.Extensions
+{
+    public static class ScriptRegisterExtensions
+    {
+        public static void IncludePluginScript(this ScriptRegister register, string script, int? order = null)
+        {
+            string path = string.Format("/Plugins/Plugins.Widgets.Google/Scripts/{0}", script);
+            register.IncludeExternal(path, order);
+        }
+    }
+}

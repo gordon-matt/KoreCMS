c9c35369c17c38ded114acfcde5c65db10390221
MattEsales <gordon-matt@users.noreply.github.com>
2015-05-25 02:52:42 +0000

    Can now export language packs (canot import yet though) and also added 5 new themes: Darkly, Lumen, Paper, Sandstone and Superhero

--- Kore.Web.ContentManagement/Areas/Admin/Localization/Controllers/LocalizableStringController.cs
+++ Kore.Web.ContentManagement/Areas/Admin/Localization/Controllers/LocalizableStringController.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 using System.Web.Mvc;
 using Kore.Localization.Services;
+using Kore.Web.Configuration;
+using Kore.Web.ContentManagement.Areas.Admin.Localization.Models;
 using Kore.Web.Mvc;
 using Kore.Web.Security.Membership.Permissions;
 
@@ -12,10 +16,17 @@ namespace Kore.Web.ContentManagement.Areas.Admin.Localization.Controllers
     public class LocalizableStringController : KoreController
     {
         private readonly Lazy<ILanguageService> languageService;
+        private readonly Lazy<ILocalizableStringService> localizableStringService;
+        private readonly KoreSiteSettings siteSettings;
 
-        public LocalizableStringController(Lazy<ILanguageService> languageService)
+        public LocalizableStringController(
+            Lazy<ILanguageService> languageService,
+            Lazy<ILocalizableStringService> localizableStringService,
+            KoreSiteSettings siteSettings)
         {
             this.languageService = languageService;
+            this.localizableStringService = localizableStringService;
+            this.siteSettings = siteSettings;
         }
 
         [Route("{languageId}")]
@@ -40,8 +51,26 @@ namespace Kore.Web.ContentManagement.Areas.Admin.Localization.Controllers
             return View("Kore.Web.ContentManagement.Areas.Admin.Localization.Views.LocalizableString.Index");
         }
 
+        [Route("export/{cultureCode}")]
+        public ActionResult ExportLanguagePack(string cultureCode)
+        {
+            var localizedStrings = localizableStringService.Value.Find(x =>
+                x.CultureCode == cultureCode &&
+                x.TextValue != null);
+
+            var languagePack = new LanguagePackFile
+            {
+                CultureCode = cultureCode,
+                LocalizedStrings = localizedStrings.ToDictionary(k => k.TextKey, v => v.TextValue)
+            };
+
+            string json = languagePack.ToJson();
+            string fileName = string.Format("{0}_LanguagePack_{1}_{2:yyyy-MM-dd}.json", siteSettings.SiteName, cultureCode, DateTime.Now);
+            return File(new UTF8Encoding().GetBytes(json), "application/json", fileName);
+        }
+
         //TODO: Test
-        [Route("translate/{text}")]
+        [Route("translate/{key}")]
         public JsonResult Translate(string key)
         {
             return Json(new { Translation = T(key).Text });

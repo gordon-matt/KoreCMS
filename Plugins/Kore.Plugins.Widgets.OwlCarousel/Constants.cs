184537bda68d18239d63555cc639e4c7691e373e
MattEsales <gordon-matt@users.noreply.github.com>
2015-07-02 09:21:00 +0000

    Owl Carousel Plugin

---
+++ Plugins/Kore.Plugins.Widgets.OwlCarousel/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Kore.Plugins.Widgets.OwlCarousel
+{
+    public static class Constants
+    {
+        public const string PluginSystemName = "Kore.Plugins.Widgets.OwlCarousel";
+        public const string RouteArea = "Plugins/Widgets/OwlCarousel";
+    }
+}
---
+++ Plugins/Kore.Plugins.Widgets.OwlCarousel/Infrastructure/LocationFormatProvider.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections.Generic;
+using Kore.Web.Mvc.Themes;
+
+namespace Kore.Plugins.Widgets.OwlCarousel.Infrastructure
+{
+    public class LocationFormatProvider : ILocationFormatProvider
+    {
+        #region ILocationFormatProvider Members
+
+        public IEnumerable<string> AreaViewLocationFormats
+        {
+            get
+            {
+                return new[]
+                {
+                    //themes
+                    "~/Areas/{2}/Themes/{3}/Views/Plugins/Widgets.OwlCarousel/{1}/{0}.cshtml",
+                    "~/Areas/{2}/Themes/{3}/Views/Plugins/Widgets.OwlCarousel/Shared/{0}.cshtml",
+
+                    //shared
+                    "~/Areas/{2}/Views/Plugins/Widgets.OwlCarousel/{1}/{0}.cshtml",
+                    "~/Areas/{2}/Views/Plugins/Widgets.OwlCarousel/Shared/{0}.cshtml",
+
+                    //default
+                    "~/Plugins/Widgets.OwlCarousel/Areas/{2}/Views/{1}/{0}.cshtml",
+                    "~/Plugins/Widgets.OwlCarousel/Areas/{2}/Views/Shared/{0}.cshtml",
+                };
+            }
+        }
+
+        public IEnumerable<string> AreaMasterLocationFormats
+        {
+            get
+            {
+                return new[]
+                {
+                    //themes
+                    "~/Areas/{2}/Themes/{3}/Views/Plugins/Widgets.OwlCarousel/{1}/{0}.cshtml",
+                    "~/Areas/{2}/Themes/{3}/Views/Plugins/Widgets.OwlCarousel/Shared/{0}.cshtml",
+
+                    //shared
+                    "~/Areas/{2}/Views/Plugins/Widgets.OwlCarousel/{1}/{0}.cshtml",
+                    "~/Areas/{2}/Views/Plugins/Widgets.OwlCarousel/Shared/{0}.cshtml",
+
+                    //default
+                    "~/Plugins/Widgets.OwlCarousel/Areas/{2}/Views/{1}/{0}.cshtml",
+                    "~/Plugins/Widgets.OwlCarousel/Areas/{2}/Views/Shared/{0}.cshtml",
+                };
+            }
+        }
+
+        public IEnumerable<string> AreaPartialViewLocationFormats
+        {
+            get
+            {
+                return new[]
+                {
+                    //themes
+                    "~/Areas/{2}/Themes/{3}/Views/Plugins/Widgets.OwlCarousel/{1}/{0}.cshtml",
+                    "~/Areas/{2}/Themes/{3}/Views/Plugins/Widgets.OwlCarousel/Shared/{0}.cshtml",
+
+                    //shared
+                    "~/Areas/{2}/Views/Plugins/Widgets.OwlCarousel/{1}/{0}.cshtml",
+                    "~/Areas/{2}/Views/Plugins/Widgets.OwlCarousel/Shared/{0}.cshtml",
+
+                    //default
+                    "~/Plugins/Widgets.OwlCarousel/Areas/{2}/Views/{1}/{0}.cshtml",
+                    "~/Plugins/Widgets.OwlCarousel/Areas/{2}/Views/Shared/{0}.cshtml",
+                };
+            }
+        }
+
+        public IEnumerable<string> ViewLocationFormats
+        {
+            get
+            {
+                return new[]
+                {
+                    //themes
+                    "~/Themes/{2}/Views/Plugins/Widgets.OwlCarousel/{1}/{0}.cshtml",
+                    "~/Themes/{2}/Views/Plugins/Widgets.OwlCarousel/Shared/{0}.cshtml",
+
+                    //shared
+                    "~/Views/Plugins/Widgets.OwlCarousel/{1}/{0}.cshtml",
+                    "~/Views/Plugins/Widgets.OwlCarousel/Shared/{0}.cshtml",
+
+                    //default
+                    "~/Plugins/Widgets.OwlCarousel/Views/{1}/{0}.cshtml",
+                    "~/Plugins/Widgets.OwlCarousel/Views/Shared/{0}.cshtml",
+                };
+            }
+        }
+
+        public IEnumerable<string> MasterLocationFormats
+        {
+            get
+            {
+                return new[]
+                {
+                    //themes
+                    "~/Themes/{2}/Views/Plugins/Widgets.OwlCarousel/{1}/{0}.cshtml",
+                    "~/Themes/{2}/Views/Plugins/Widgets.OwlCarousel/Shared/{0}.cshtml",
+
+                    //shared
+                    "~/Views/Plugins/Widgets.OwlCarousel/{1}/{0}.cshtml",
+                    "~/Views/Plugins/Widgets.OwlCarousel/Shared/{0}.cshtml",
+
+                    //default
+                    "~/Plugins/Widgets.OwlCarousel/Views/{1}/{0}.cshtml",
+                    "~/Plugins/Widgets.OwlCarousel/Views/Shared/{0}.cshtml",
+                };
+            }
+        }
+
+        public IEnumerable<string> PartialViewLocationFormats
+        {
+            get
+            {
+                return new[]
+                {
+                    //themes
+                    "~/Themes/{2}/Views/Plugins/Widgets.OwlCarousel/{1}/{0}.cshtml",
+                    "~/Themes/{2}/Views/Plugins/Widgets.OwlCarousel/Shared/{0}.cshtml",
+
+                    //shared
+                    "~/Views/Plugins/Widgets.OwlCarousel/{1}/{0}.cshtml",
+                    "~/Views/Plugins/Widgets.OwlCarousel/Shared/{0}.cshtml",
+
+                    //default
+                    "~/Plugins/Widgets.OwlCarousel/Views/{1}/{0}.cshtml",
+                    "~/Plugins/Widgets.OwlCarousel/Views/Shared/{0}.cshtml",
+                };
+            }
+        }
+
+        #endregion ILocationFormatProvider Members
+    }
+}

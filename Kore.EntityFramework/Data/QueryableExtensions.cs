024d4646295af64c424d15e682d62956ae87eef5
MattEsales <MattEsales@5994fa0f-9bc0-4ba1-bc9c-409c9fb3efa9>
2015-11-20 23:57:49 +0000

    Added Async methods to IRepository<T> and IGenericDataService<T>

---
+++ Kore.EntityFramework/Data/QueryableExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kore.EntityFramework.Data
+{
+    public static class QueryableExtensions
+    {
+        public static Task<HashSet<TSource>> ToHashSetAsync<TSource>(this IQueryable<TSource> source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            return IDbAsyncEnumerableExtensions.ToHashSetAsync<TSource>(QueryableExtensions.AsDbAsyncEnumerable<TSource>(source));
+        }
+
+        private static IDbAsyncEnumerable<T> AsDbAsyncEnumerable<T>(this IQueryable<T> source)
+        {
+            IDbAsyncEnumerable<T> dbAsyncEnumerable = source as IDbAsyncEnumerable<T>;
+            if (dbAsyncEnumerable != null)
+            {
+                return dbAsyncEnumerable;
+            }
+
+            throw new InvalidOperationException("IQueryable not async!");
+        }
+    }
+}
---
+++ Kore.EntityFramework/Data/TaskExtensions.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kore.EntityFramework.Data
+{
+    internal static class TaskExtensions
+    {
+        public static TaskExtensions.CultureAwaiter<T> WithCurrentCulture<T>(this Task<T> task)
+        {
+            return new TaskExtensions.CultureAwaiter<T>(task);
+        }
+
+        public static TaskExtensions.CultureAwaiter WithCurrentCulture(this Task task)
+        {
+            return new TaskExtensions.CultureAwaiter(task);
+        }
+
+        public struct CultureAwaiter<T> : ICriticalNotifyCompletion, INotifyCompletion
+        {
+            private readonly Task<T> _task;
+
+            public bool IsCompleted
+            {
+                get
+                {
+                    return this._task.IsCompleted;
+                }
+            }
+
+            public CultureAwaiter(Task<T> task)
+            {
+                this._task = task;
+            }
+
+            public TaskExtensions.CultureAwaiter<T> GetAwaiter()
+            {
+                return this;
+            }
+
+            public T GetResult()
+            {
+                return this._task.GetAwaiter().GetResult();
+            }
+
+            public void OnCompleted(Action continuation)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void UnsafeOnCompleted(Action continuation)
+            {
+                CultureInfo currentCulture = Thread.CurrentThread.CurrentCulture;
+                this._task.ConfigureAwait(false).GetAwaiter().UnsafeOnCompleted((Action)(() =>
+                {
+                    CultureInfo currentCulture1 = Thread.CurrentThread.CurrentCulture;
+                    Thread.CurrentThread.CurrentCulture = currentCulture;
+                    try
+                    {
+                        continuation();
+                    }
+                    finally
+                    {
+                        Thread.CurrentThread.CurrentCulture = currentCulture1;
+                    }
+                }));
+            }
+        }
+
+        public struct CultureAwaiter : ICriticalNotifyCompletion, INotifyCompletion
+        {
+            private readonly Task _task;
+
+            public bool IsCompleted
+            {
+                get
+                {
+                    return this._task.IsCompleted;
+                }
+            }
+
+            public CultureAwaiter(Task task)
+            {
+                this._task = task;
+            }
+
+            public TaskExtensions.CultureAwaiter GetAwaiter()
+            {
+                return this;
+            }
+
+            public void GetResult()
+            {
+                this._task.GetAwaiter().GetResult();
+            }
+
+            public void OnCompleted(Action continuation)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void UnsafeOnCompleted(Action continuation)
+            {
+                CultureInfo currentCulture = Thread.CurrentThread.CurrentCulture;
+                this._task.ConfigureAwait(false).GetAwaiter().UnsafeOnCompleted((Action)(() =>
+                {
+                    CultureInfo currentCulture1 = Thread.CurrentThread.CurrentCulture;
+                    Thread.CurrentThread.CurrentCulture = currentCulture;
+                    try
+                    {
+                        continuation();
+                    }
+                    finally
+                    {
+                        Thread.CurrentThread.CurrentCulture = currentCulture1;
+                    }
+                }));
+            }
+        }
+    }
+}

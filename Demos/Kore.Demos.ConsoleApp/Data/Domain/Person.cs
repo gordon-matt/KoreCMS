4fbe8d4b3831be353f0d21b029369af72a98b329
MattEsales <MattEsales@5994fa0f-9bc0-4ba1-bc9c-409c9fb3efa9>
2016-11-25 07:05:04 +0000

    Console demo

---
+++ Demos/Kore.Demos.ConsoleApp/Data/Domain/Person.cs
@@ -0,0 +1,44 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using Kore.Data;
+using Kore.Data.EntityFramework;
+
+namespace Kore.Demos.ConsoleApp.Data.Domain
+{
+    public class Person : IEntity
+    {
+        public int Id { get; set; }
+
+        public string FamilyName { get; set; }
+
+        public string GivenNames { get; set; }
+
+        #region IEntity Members
+
+        public object[] KeyValues
+        {
+            get { return new object[] { Id }; }
+        }
+
+        #endregion IEntity Members
+    }
+
+    public class PersonMap : EntityTypeConfiguration<Person>, IEntityTypeConfiguration
+    {
+        public PersonMap()
+        {
+            ToTable("People");
+            HasKey(x => x.Id);
+            Property(x => x.FamilyName).IsRequired().HasMaxLength(128).IsUnicode(true);
+            Property(x => x.GivenNames).IsRequired().HasMaxLength(128).IsUnicode(true);
+        }
+
+        #region IEntityTypeConfiguration Members
+
+        public bool IsEnabled
+        {
+            get { return true; }
+        }
+
+        #endregion IEntityTypeConfiguration Members
+    }
+}
---
+++ Demos/Kore.Demos.ConsoleApp/Infrastructure/AutofacRequestLifetimeHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Autofac;
+
+namespace Kore.Demos.ConsoleApp.Infrastructure
+{
+    public static class AutofacRequestLifetimeHelper
+    {
+        private static ILifetimeScope lifetimeScope;
+        private static readonly object RequestTag = "AutofacConsoleRequest";
+
+        public static ILifetimeScope GetLifetimeScope(ILifetimeScope container, Action<ContainerBuilder> configurationAction)
+        {
+            return lifetimeScope ?? (lifetimeScope = InitializeLifetimeScope(configurationAction, container));
+        }
+
+        private static ILifetimeScope LifetimeScope
+        {
+            get { return lifetimeScope; }
+            set { lifetimeScope = value; }
+        }
+
+        private static ILifetimeScope InitializeLifetimeScope(Action<ContainerBuilder> configurationAction, ILifetimeScope container)
+        {
+            return (configurationAction == null)
+                ? container.BeginLifetimeScope(RequestTag)
+                : container.BeginLifetimeScope(RequestTag, configurationAction);
+        }
+
+        public static void Dispose()
+        {
+            if (lifetimeScope != null)
+            {
+                lifetimeScope.Dispose();
+            }
+        }
+    }
+}

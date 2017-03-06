a6ea1639534643299ca6dec214294b548a1161c2
vnmatt <gordon_matt@hotmail.com>
2017-03-06 17:01:10 +1030

    Change for LocalizedDisplayNameAttribute

--- Kore/ComponentModel/LocalizedDisplayNameAttribute.cs
+++ Kore/ComponentModel/LocalizedDisplayNameAttribute.cs
@@ -1,9 +1,11 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using Kore.Localization;
 
 namespace Kore.ComponentModel
 {
     //TODO: Implement this with a custom DataAnnotationsModelMetadataProvider
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
     public class LocalizedDisplayNameAttribute : DisplayNameAttribute//, IModelAttribute
     {
         private static Localizer localizer;
